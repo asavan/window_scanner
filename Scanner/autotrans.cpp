b05484fb144e262f0aa7453311b4149660a758e9
asavan <asavan@mail.ru>
2015-02-03 19:38:29 +0300

    fix tests

--- Scanner/autotrans.cpp
+++ Scanner/autotrans.cpp
@@ -5,11 +5,13 @@ int main(int argc, char* argv[])
     Storage* storage = NULL;
     if (argc < 3)
     {
-        std::cout << "Default arguments are used" << std::endl;
-        storage = new Storage("1680x1050/layout", "1680x1050");
+        std::cout << "Default arguments" << std::endl;
+        storage = new Storage("../../base/1680x1050/layout", "../../base/1680x1050");
     }
     else
+	{
         storage = new Storage(argv[1], argv[2]);
+	}
 
 //    storage->test__("tempbase/1680x1050/layout", "tempbase/1680x1050");
     storage->test__("1680x1050/layout", "1680x1050");
